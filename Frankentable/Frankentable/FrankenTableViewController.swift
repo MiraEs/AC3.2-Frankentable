a9df82fc21d509ce71a9f7077d1d5f5441df248b
Mira Estil <imz.estil@gmail.com>
2016-11-27 22:45:11 -0500

    Frankenstein HW - filtering repeated words

--- Frankentable/Frankentable/FrankenTableViewController.swift
+++ Frankentable/Frankentable/FrankenTableViewController.swift
@@ -9,89 +9,78 @@
 import UIKit
 
 class FrankenTableViewController: UITableViewController {
-
+    
+    var repeatedWordsAndFreq = [String:Int]()
+    var repeatedWords = [String]()
+    var frankText = ""
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        getText()
+        repeatingWords(input: frankText)
+        
+    }
+    
+    func getText () {
         if let url = Bundle.main.url(forResource: "Data", withExtension: "txt"),
             let data = try? Data(contentsOf: url),
             let text = String(data: data, encoding: .utf8) {
-            
             // here's your text
+            frankText = text
             print(text)
         }
     }
+    
+    func repeatingWords(input: String) {
+        var allTheStuff = CharacterSet()
+        allTheStuff.insert(charactersIn: ".,?!()")
+        allTheStuff.insert(charactersIn: " ")
+        let wordArray = input.lowercased().components(separatedBy: allTheStuff).filter{$0 != ""}
+        
+        var frequencyDict = [String: Int]()
+        
+        for word in wordArray {
+            frequencyDict[word] = (frequencyDict[word] ?? 0) + 1 //?
+        }
+        
+        let sortedArr = frequencyDict.keys.sorted{frequencyDict[$0]! > frequencyDict[$1]!}
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        repeatedWordsAndFreq = frequencyDict
+        repeatedWords = sortedArr
+        //dump(repeatedWords)
+        dump(repeatedWordsAndFreq)
+        
     }
+  
 
     // MARK: - Table view data source
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return repeatedWords.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
+        
+        var arrofRepeats = [(String, Int)]()
+        for dict in repeatedWordsAndFreq {
+            arrofRepeats.append((dict.key, dict.value))
+        }
+        
+          cell.textLabel?.text = "\(arrofRepeats[indexPath.row].0)  (\(arrofRepeats[indexPath.row].1))"
+        
+        
+        
+            //cell.textLabel?.text = repeatedWords[indexPath.row]
         return cell
     }
-    */
+ 
 
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
